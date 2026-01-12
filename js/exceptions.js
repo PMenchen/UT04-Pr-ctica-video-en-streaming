933fd06f83442ca1936a9b0fdd4224ebb935aba9
PMenchen <72562238+PMenchen@users.noreply.github.com>
2026-01-12 17:25:15 +0100

    Inserción de excepciones para el proyecto

--- js/exceptions.js
+++ js/exceptions.js
@@ -58,3 +58,91 @@ function InvalidValueException(param, expected, value) {
 }
 InvalidValueException.prototype = Object.create(BaseException.prototype);
 InvalidValueException.prototype.constructor = InvalidValueException;
+
+
+function CategoryExistsException(category){
+    let instance = BaseException.call(this, `Error: La categoría ${category.name} ya existe en el sistema`);
+    instance.name = "CategoryExistsException";
+    instance.param = category;
+
+    return instance;
+}
+CategoryExistsException.prototype = Object.create(BaseException.prototype);
+CategoryExistsException.prototype.constructor = CategoryExistsException;
+
+
+function CategoryNotExistsException(category){
+    let instance = BaseException.call(this, `Error: La categoría ${category.name} no está registrada en el sistema`);
+    instance.name = "CategoryNotExistsException";
+    instance.param = category;
+
+    return instance;
+}
+CategoryNotExistsException.prototype = Object.create(BaseException.prototype);
+CategoryNotExistsException.prototype.constructor = CategoryNotExistsException;
+
+
+function UserExistsException(user){
+    let instance = BaseException.call(this, `Error: El usuario ${user.username} ya existe en el sistema`);
+    instance.name = "UserExistsException";
+    instance.param = user;
+
+    return instance;
+}
+UserExistsException.prototype = Object.create(BaseException.prototype);
+UserExistsException.prototype.constructor = UserExistsException;
+
+
+function UserNotExistsException(user){
+    let instance = BaseException.call(this, `Error: El usuario ${user.username} no existe en el sistema`);
+    instance.name = "UserNotExistsException";
+    instance.param = user;
+
+    return instance;
+}
+UserNotExistsException.prototype = Object.create(BaseException.prototype);
+UserNotExistsException.prototype.constructor = UserNotExistsException;
+
+
+function ProductionExistsException(production){
+    let instance = BaseException.call(this, `Error: La producción ${production.title} ya existe en el sistema`);
+    instance.name = "ProductionExistsException";
+    instance.param = production;
+
+    return instance;
+}
+ProductionExistsException.prototype = Object.create(BaseException.prototype);
+ProductionExistsException.prototype.constructor = ProductionExistsException;
+
+
+function ProductionNotExistsException(production){
+    let instance = BaseException.call(this, `Error: La producción ${production.title} no está registrada en el sistema`);
+    instance.name = "ProductionNotExistsException";
+    instance.param = production;
+
+    return instance;
+}
+ProductionNotExistsException.prototype = Object.create(BaseException.prototype);
+ProductionNotExistsException.prototype.constructor = ProductionNotExistsException;
+
+
+function PersonExistsException(person){
+    let instance = BaseException.call(this, `Error: La persona ${person.name} ${person.lastname1} ya existe en el sistema`);
+    instance.name = "PersonExistsException";
+    instance.param = person;
+
+    return instance;
+}
+PersonExistsException.prototype = Object.create(BaseException.prototype);
+PersonExistsException.prototype.constructor = PersonExistsException;
+
+
+function PersonNotExistsException(person){
+    let instance = BaseException.call(this, `Error: La persona ${person.name} ${person.lastname1} no existe en el sistema`);
+    instance.name = "PersonNotExistsException";
+    instance.param = person;
+
+    return instance;
+}
+PersonNotExistsException.prototype = Object.create(BaseException.prototype);
+PersonNotExistsException.prototype.constructor = PersonNotExistsException;
