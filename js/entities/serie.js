c812ac94e25d98043290e2fa19e27f59da275b0d
PMenchen <72562238+PMenchen@users.noreply.github.com>
2026-01-12 14:20:36 +0100

    Creación objeto Serie

---
+++ js/entities/serie.js
@@ -0,0 +1,66 @@
+"use strict";
+
+import Production from "./production";
+
+class Serie extends Production{
+    #resources
+    #locations
+    #seasons
+
+    constructor(title, publication, nationality = "", synopsis = "", image = "", resources = [], locations = [], seasons = 0) {
+        super(title, publication, nationality, synopsis, image);
+        this.#resources = resources;
+        this.#locations = locations;
+        this.#seasons = seasons;
+    }
+
+    // Getters
+    get resources() {
+        return this.#resources;
+    }
+
+    get locations() {
+        return this.#locations;
+    }
+
+    get seasons() {
+        return this.#seasons;
+    }
+
+    // Setters
+    set resources(value) {
+        if (!Array.isArray(value)) {
+            throw new InvalidValueException("resources", "array", value);
+        }
+        this.#resources = value;
+    }
+
+    set locations(value) {
+        if (!Array.isArray(value)) {
+            throw new InvalidValueException("locations", "array", value);
+        }
+        this.#locations = value;
+    }
+
+    set seasons(value) {
+        if (typeof value !== "number" || Number.isNaN(value)) {
+            throw new InvalidValueException("seasons", "number", value);
+        }
+        this.#seasons = value;
+    }
+
+    // Métodos auxiliares
+    addResource(resource) {
+        this.#resources.push(resource);
+    }
+
+    addLocation(coordinate) {
+        this.#locations.push(coordinate);
+    }
+
+    toString() {
+        return `[Serie] ${super.toString()}${this.#seasons > 0 ? " - " + this.#seasons + " temporadas" : ""}`;
+    }
+}
+
+export default Serie;
